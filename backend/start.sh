997db2d88eeaf85e19e9f3298d902ef6d0eace11
Sathishkumar <106817617+sathishkumart2001@users.noreply.github.com>
2025-10-28 14:35:28 +0530

    Update start.sh

--- backend/start.sh
+++ backend/start.sh
@@ -10,4 +10,4 @@ fi
 # Default port
 PORT=${PORT:-8000}
 
-exec gunicorn -k uvicorn.workers.UvicornWorker app.main:app --bind 0.0.0.0:${PORT} --workers 1 --log-level info
+exec gunicorn -k uvicorn.workers.UvicornWorker backend.app.main:app --bind 0.0.0.0:${PORT} --workers 1 --log-level info
